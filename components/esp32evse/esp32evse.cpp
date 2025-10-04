34a9ce4509255d10d3a754e093578176ce011251
H. Árkosi Róbert <robreg@zsurob.hu>
2025-10-04 13:03:05 +0200

    Update esp32evse.cpp

--- components/esp32evse/esp32evse.cpp
+++ components/esp32evse/esp32evse.cpp
@@ -710,11 +710,12 @@ void ESP32EVSEComponent::process_line_(const std::string &line) {
     return;
   }
   if (line == "ERROR") {
+    ESP_LOGW(TAG, "ESP32-EVSE error response received");
     this->handle_ack_(false);
     return;
   }
   if (line == "RDY") {
-    ESP_LOGI(TAG, "EVSE ready to accept commands");
+    ESP_LOGI(TAG, "ESP32-EVSE ready to accept commands");
     if (this->ready_trigger_ != nullptr)
       this->ready_trigger_->trigger();
     return;
